a28d281add0c3d1b7487dbd6d29390772dbfe37b
Luca Santiago Latigano <lucalatigano12@gmail.com>
2025-10-15 15:33:47 -0300

    todo listo pal tp8 papa

--- src/EJ10.java
+++
@@ -1,9 +0,0 @@
-public class EJ10 {
-    public static void main(String[] args) {
-        clsColaLinkedList objCola = new clsColaLinkedList();
-        clsColaLinkedList objColaEspejo = new clsColaLinkedList();
-        objCola.meterChar("Bond, James Bond");
-        objCola.Espejo(objColaEspejo);
-        objColaEspejo.muestra();
-    }
-}
